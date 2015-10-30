d44c1ce7bdb6b03d949927e9d77caefdf58bf17d
Beauclair_B <laouningoue@gmail.com>
2015-10-30 13:46:33 +0100

    réintégration des packages

---
+++ src/controller/visiteurs/EntreprisesAction.java
@@ -0,0 +1,14 @@
+package controller.visiteurs;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class EntreprisesAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1787968901840180823L;
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+
+}
