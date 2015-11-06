44266fd3eeff89c385cb17d7c78f6437a435f2fa
Beauclair_B <laouningoue@gmail.com>
2015-11-06 15:01:18 +0100

    pareil

---
+++ src/controller/commun/detailOffreAction.java
@@ -0,0 +1,13 @@
+package controller.commun;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class detailOffreAction extends ActionSupport{
+	
+	private static final long serialVersionUID = 5600425158593800271L;
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
