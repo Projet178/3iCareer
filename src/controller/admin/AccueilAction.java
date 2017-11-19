807226b10c3562360d861ea7cda21a2d486a35a4
Laouni <laouningoue@gmail.com>
2017-11-19 03:03:49 +0100

    Commit de test
    Test de commit avec GitHub

--- src/controller/admin/AccueilAction.java
+++ src/controller/admin/AccueilAction.java
@@ -6,6 +6,8 @@ public class AccueilAction extends ActionSupport{
 	
 	/** Id de sérialisation */
 	private static final long serialVersionUID = -5664697474450332777L;
+	private Integer maVariable;
+	private  String monString;
 
 	public String execute() throws Exception {
 		return SUCCESS;
